341d5ccd6b8df3188096fa93d72a6e7204060dbd
Suman Saurabh <108397606+GITsumansaurabh@users.noreply.github.com>
2023-11-24 21:52:47 +0530

    Add files via upload

---
+++ src/pages/Home1.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Header from '../features/header-main/Header'
+
+export default function Home1() {
+  return (
+    <div>
+    <Header></Header>
+    </div>
+  )
+}
